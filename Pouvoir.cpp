e73b8d60ab8da3b6be127d052f6f44ba5e25a4c0
SimonRiopel <98927317+SimonRiopel@users.noreply.github.com>
2022-02-04 10:33:36 -0500

    Update Pouvoir.cpp

--- Pouvoir.cpp
+++ Pouvoir.cpp
@@ -36,21 +36,27 @@ unsigned int Pouvoir::obtenirNombreDeDegat() const
 	return nombreDeDegat_;
 }
 
+
+
 // TODO: Augmenter le nombre de degats de 10.
 Pouvoir& Pouvoir::operator++(int){
-  
+	this->nombreDeDegat_ = this->nombreDeDegat_ + 10;
     return  *this;
 }
 
 // TODO: Comparer le nom des pouvoirs
-bool Pouvoir::operator==(const Pouvoir& pouvoir) const
-{
-	return false;
+bool Pouvoir::operator==(const Pouvoir& pouvoir) const {
+	bool egal = false;
+	if (nom_ == pouvoir.nom_) {
+		egal = true;
+	}
+	return egal;
 }
 
 // TODO: Afficher le nom du pouvoir, le nombre de degats
 // et l'energie necessaire.
 ostream& operator<<(ostream& os, const Pouvoir& pouvoir)
 {
+	os << pouvoir.obtenirNom() << " possede un nombre de degats de " << pouvoir.obtenirNombreDeDegat() << " et une energie necessaire de " << pouvoir.obtenirEnergieNecessaire() << endl;
 	 return os;
 }
